4546fd4c80b4c227961820cfcb05cc0b4be5edb3
J3niff3r Guañuna <jeny.guanuna@gmail.com>
2021-07-26 05:09:03 -0500

    rutas books

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//return $request->user();
+//});
+
+Route::get('projects/{project}', function (){
+    return ['project1','project2']; 
+});
+Route::post('projects', function (){
+    return "creado"; 
+});
+Route::put('projects/{project}', function (){
+    return "Actualizado"; 
+});
+Route::delete('projects/{project}', function (){
+    return "Eliminado"; 
+});
+
+//--------------
+Route::get('books/{book}/title', function (){
+    return ['book1, pride and prejuice','the train girl'];
+});
+Route::get('books/{book}/title/{title}', function (){
+    return ['book1, pride and prejuice','book2, the train girl'];
+});
+Route::post('books/{book}/title', function (){
+    return "Creado";
+});
+Route::put('books/{book}/title/{title}', function (){
+    return "Actualizado";
+});
+Route::delete('books/{book}/title/{title}', function (){
+    return "Eliminado";
 });
